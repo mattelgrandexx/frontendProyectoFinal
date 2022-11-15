d7615402eacea9179a903ae91248e9bb0f62207e
nicoelias997 <nicoelias997@gmail.com>
2022-11-15 21:04:57 +0100

    Me funciona la funcion para iniciar sesion y me pinta correctamente los datos

--- src/components/views/IniciarSesion.jsx
+++ src/components/views/IniciarSesion.jsx
@@ -1,36 +1,114 @@
-import React from 'react'
-import { Button, Card, Form } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Button, Card, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { consultarUserApi } from "../helpers/queriesLogin";
 
 const IniciarSesion = () => {
-  return (
+
+  const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (datos) => {
+    console.log(datos)
+    consultarUserApi().then((respuesta) => {
     
+      const encontrarEmail = respuesta.find(
+        (user) => user.email === datos.email
+      );
+      if (encontrarEmail) {
+        if (encontrarEmail.pass === datos.pass) {
+          Swal.fire(
+            "Bienvenido",
+            `Gracias por contar con nosotros, ${encontrarEmail.nombreUsuario}`,
+            "success"
+          );
+          navigate("/");
+        } else {
+          Swal.fire(
+            "Error",
+            `Contraseña incorrecta, vuelva a intentarlo`,
+            "error"
+          );
+        }
+      } else {
+        Swal.fire(
+          "Usuario o email incorrecto",
+          `No encontramos un usuario o email con ese nombre, vuelve a intentarlo`,
+          "error"
+        );
+      }
+    });
+  };
+
+
+  return (
     <Card>
-      <Card.Header className='bgColor p-4 m-1 text-center text-white'>
-      <Card.Title className='textLogin'>
-        INICIA SESIÓN
-      </Card.Title>
+      <Card.Header className="bgColor p-4 m-1 text-center text-white">
+        <Card.Title className="textLogin">INICIA SESIÓN</Card.Title>
       </Card.Header>
-      <Card.Body className='mt-4'>
-        <Form>
-          <Form.Group  className='mt-4'>
-            <Form.Label className='textForm'>Correo electronico*</Form.Label>
-            <Form.Control className='p-2 borderInput'>
-            </Form.Control>
+      <Card.Body className="mt-4">
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group className="mt-4">
+            <Form.Label className="textForm">Correo electronico*</Form.Label>
+            <Form.Control className="p-2 borderInput"
+            type="email"
+            {...register("email", {
+              required: "Debe ingresar un email",
+              pattern: {
+                value:
+                  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+                message: "Debe ingresar un formato valido",
+              }
+            })}
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            ></Form.Control>
+             <Form.Text className="text-danger mb-2">
+              {errors.email?.message}
+            </Form.Text>
           </Form.Group>
-          <Form.Group  className='mt-4 mb-2'>
-            <Form.Label className='textForm'>Contraseña*</Form.Label>
-            <Form.Control className='p-2 borderInput'>
-            </Form.Control>
+          <Form.Group className="mt-4 mb-2">
+            <Form.Label className="textForm">Contraseña*</Form.Label>
+            <Form.Control className="p-2 borderInput"
+            type="password"
+            {...register("password", {
+              required: "Debe ingresar una contraseña",
+              minLength: {
+                value: 8,
+                message: "Su contraseña debe tener al menos 8 caracteres",
+              },
+              maxLength: {
+                value: 30,
+                message:
+                  "Su contraseña debe tener como 30 caracteres como maximo",
+              }
+            })}
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            ></Form.Control>
+            <Form.Text className="text-danger mb-2">
+                {errors.password?.message}
+              </Form.Text>
           </Form.Group>
-          <Button className='mt-4 btnEntrar' variant='none'>
+          <Button className="mt-4 btnEntrar" variant="none" type="submit">
             Entrar
           </Button>
         </Form>
       </Card.Body>
-        <Button className='textPass' variant='none'>No recuerdo mi contraseña</Button>
+      <Button className="textPass" variant="none" type="submit">
+        No recuerdo mi contraseña
+      </Button>
     </Card>
+  );
+};
 
-  )
-}
-
-export default IniciarSesion
+export default IniciarSesion;
--- src/components/views/Registrarse.jsx
+++ src/components/views/Registrarse.jsx
@@ -1,15 +1,12 @@
-import React, { useState } from 'react'
-import { Form, Card, Button, Row, Col } from 'react-bootstrap'
-import { useForm } from 'react-hook-form'
-import { useNavigate } from 'react-router-dom'
-import Swal from 'sweetalert2'
-import { crearUserApi } from '../helpers/queriesLogin'
-
-
+import React, { useState } from "react";
+import { Form, Card, Button, Row, Col } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { crearUserApi } from "../helpers/queriesLogin";
 
 const Registrarse = () => {
-
-const navigate = useNavigate()
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -19,26 +16,16 @@ const navigate = useNavigate()
   const [usuarios, setUsuarios] = useState([]);
 
   const [nombreUsuario, setNombreUsuario] = useState("");
-  const [apellidoUsuario, setApellidoUsuario] = useState("")
+  const [apellidoUsuario, setApellidoUsuario] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const onSubmit = (datos) => {
-    const verificacionNombre = usuarios.find(
-      (usuario) => usuario.id === datos.usuario
-    );
     const verificacionEmail = usuarios.find(
       (usuario) => usuario.email === datos.email
     );
 
-    if (verificacionNombre) {
-      Swal.fire(
-        "Este usuario es existente",
-        "Prueba registrarte con otro nombre de usuario",
-        "error"
-      );
-      return;
-    } else if (verificacionEmail) {
+    if (verificacionEmail) {
       Swal.fire(
         "Este email es existente",
         "Prueba registrarte con otro email",
@@ -47,158 +34,219 @@ const navigate = useNavigate()
       return;
     } else {
       crearUserApi(datos).then((respuesta) => {
-        if(respuesta.status === 201){
-        setUsuarios([
-          ...usuarios,
-          {
-            nombreUsuario: datos.nombreUsuario,
-            apellidoUsuario: datos.apellidoUsuario,
-            email: datos.email,
-            pass: datos.pass
-          },
-        ]);
-        Swal.fire(
-          `Te registraste correctamente, ${nombreUsuario}, ${apellidoUsuario}`,
-          "Inicia sesion con tu nueva cuenta.",
-          "success"
-        );
-        navigate("/login");
-      } else {
-        Swal.fire(
+        if (respuesta.status === 201) {
+          setUsuarios([
+            ...usuarios,
+            {
+              nombreUsuario: datos.nombreUsuario,
+              apellidoUsuario: datos.apellidoUsuario,
+              email: datos.email,
+              pass: datos.pass,
+            },
+          ]);
+          Swal.fire(
+            `Te registraste correctamente, ${nombreUsuario}, ${apellidoUsuario}`,
+            "Inicia sesion con tu nueva cuenta.",
+            "success"
+          );
+          navigate("/");
+        } else {
+          Swal.fire(
             `Hubo un error inesperado`,
             "Intentelo nuevamente en breve.",
             "error"
           );
-      }
-   })
+        }
+      });
     }
-}
-  
+  };
+
   return (
     <Card>
-    <Card.Header className='bgColor p-4 m-1 text-center text-white'>
-    <Card.Title className='textLogin'>
-      CREA UNA CUENTA
-    </Card.Title>
-    </Card.Header>
-    <Card.Body className='mt-4'>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Form.Group  className='mt-4'>
-          <Form.Label className='textForm'>Nombres*</Form.Label>
-          <Form.Control className='p-2 borderInput'
-          type='text'
-           {...register("nombreUsuario", {
-            required: "Debe ingresar un nombre.",
-            minLength: {
-              value: 2,
-              message: "El nombre debe tener al menos 2 caracteres",
-            },
-            maxLength: {
-              value: 30,
-              message: "El nombre no debe tener mas de 30 caracteres",
-            }
-          })}
-          onChange={(e) => setNombreUsuario(e.target.value)}
-          value={nombreUsuario}
-          >
-          </Form.Control>
-          <Form.Text className="text-danger">
-                {errors.nombreUsuario?.message}
+      <Card.Header className="bgColor p-4 m-1 text-center text-white">
+        <Card.Title className="textLogin">CREA UNA CUENTA</Card.Title>
+      </Card.Header>
+      <Card.Body className="mt-4">
+        {/* <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group className="mt-4">
+            <Form.Label className="textForm">Nombres*</Form.Label>
+            <Form.Control
+              className="p-2 borderInput"
+              type="text"
+              {...register("nombreUsuario", {
+                required: "Debe ingresar un nombre.",
+                minLength: {
+                  value: 2,
+                  message: "El nombre debe tener al menos 2 caracteres",
+                },
+                maxLength: {
+                  value: 30,
+                  message: "El nombre no debe tener mas de 30 caracteres",
+                },
+              })}
+              onChange={(e) => setNombreUsuario(e.target.value)}
+              value={nombreUsuario}
+            ></Form.Control>
+            <Form.Text className="text-danger">
+              {errors.nombreUsuario?.message}
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mt-4 mb-2">
+            <Form.Label className="textForm">Apellidos*</Form.Label>
+            <Form.Control
+              className="p-2 borderInput"
+              type="text"
+              {...register("apellidoUsuario", {
+                required: "Debe ingresar su apellido.",
+                minLength: {
+                  value: 2,
+                  message: "El nombre debe tener al menos 2 caracteres",
+                },
+                maxLength: {
+                  value: 30,
+                  message: "El nombre no debe tener mas de 30 caracteres",
+                },
+              })}
+              onChange={(e) => setApellidoUsuario(e.target.value)}
+              value={apellidoUsuario}
+            ></Form.Control>
+            <Form.Text className="text-danger">
+              {errors.apellidoUsuario?.message}
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mt-4 mb-2">
+            <Form.Label className="textForm">Correo electronico*</Form.Label>
+            <Form.Control
+              className="p-2 borderInput"
+              type="email"
+              {...register("email", {
+                required: "Debe ingresar un email",
+                pattern: {
+                  value:
+                    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+                  message: "Debe ingresar un formato valido",
+                },
+              })}
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            ></Form.Control>
+            <Form.Text className="text-danger mb-2">
+              {errors.email?.message}
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mt-4 mb-2">
+            <Form.Label className="textForm">Contraseña*</Form.Label>
+            <Form.Control
+              className="p-2 borderInput"
+              type="password"
+              {...register("password", {
+                required: "Debe ingresar una contraseña",
+                minLength: {
+                  value: 8,
+                  message: "Su contraseña debe tener al menos 8 caracteres",
+                },
+                maxLength: {
+                  value: 30,
+                  message:
+                    "Su contraseña debe tener como 30 caracteres como maximo",
+                },
+              })}
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            >
+              <Form.Text className="text-danger mb-2">
+                {errors.password?.message}
               </Form.Text>
-        </Form.Group>
-        <Form.Group  className='mt-4 mb-2'>
-          <Form.Label className='textForm'>Apellidos*</Form.Label>
-          <Form.Control className='p-2 borderInput'
-          type='text'
-          {...register("apellidoUsuario", {
-            required: "Debe ingresar su apellido.",
-            minLength: {
-              value: 2,
-              message: "El nombre debe tener al menos 2 caracteres",
-            },
-            maxLength: {
-              value: 30,
-              message: "El nombre no debe tener mas de 30 caracteres",
-            }
-          })}
-          onChange={(e) => setApellidoUsuario(e.target.value)}
-          value={apellidoUsuario}
-          >
-          </Form.Control>
-          <Form.Text className="text-danger">
-                {errors.apellidoUsuario?.message}
-              </Form.Text>
-        </Form.Group>
-        <Form.Group  className='mt-4 mb-2'>
-          <Form.Label className='textForm'>Correo electronico*</Form.Label>
-          <Form.Control className='p-2 borderInput'
-          type='email'
-           {...register("email", {
-            required: "Debe ingresar un email",
-            pattern: {
-              value:
-                /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
-              message: "Debe ingresar un formato valido",
-            },
-          })}
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          >
-          </Form.Control>
-          <Form.Text className="text-danger mb-2">
-                {errors.email?.message}
-              </Form.Text>
-        </Form.Group>
-        <Form.Group  className='mt-4 mb-2'>
-          <Form.Label className='textForm'>Contraseña*</Form.Label>
-          <Form.Control className='p-2 borderInput'
-          type="password"
-          {...register("password", {
-            required: "Debe ingresar una contraseña",
-            minLength: {
-              value: 8,
-              message: "Su contraseña debe tener al menos 8 caracteres",
-            },
-            maxLength: {
-              value: 30,
-              message:
-                "Su contraseña debe tener como 30 caracteres como maximo",
-            },
-          })}
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          >
-        <Form.Text className="text-danger mb-2">
-          {errors.password?.message}
-        </Form.Text>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group>
-          <Card.Subtitle className='mt-3 mb-3 textForm'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem deleniti sapiente alias nesciunt accusamus laborum doloribus facere culpa. Quidem omnis velit necessitatibus. Commodi, animi nesciunt. Illum consectetur amet placeat commodi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi esse maxime aspernatur libero, fuga explicabo molestias possimus consequatur enim architecto voluptates hic sequi vitae, dicta at consectetur officiis earum tenetur.
-          </Card.Subtitle>
-          <Row>
-            <Col xs={1}>
-          <Form.Check
-           {...register("check", {
-            required: "Debe aceptar los terminos"
-           })}
-          ></Form.Check>
-          </Col>
-          <Col xs={11} >
-          <p className='textForm'>
-            <strong>Acepto terminos y condiciones.</strong>
-          </p>
-          </Col>
-          </Row>
-        </Form.Group>
-        <Button className='mt-4 btnEntrar' variant='none' type='submit'>
-          Crear cuenta
-        </Button>
-      </Form>
-    </Card.Body>
-  </Card>
-  )
-}
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Card.Subtitle className="mt-3 mb-3 textForm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+              deleniti sapiente alias nesciunt accusamus laborum doloribus
+              facere culpa. Quidem omnis velit necessitatibus. Commodi, animi
+              nesciunt. Illum consectetur amet placeat commodi. Lorem ipsum
+              dolor sit amet, consectetur adipisicing elit. Excepturi esse
+              maxime aspernatur libero, fuga explicabo molestias possimus
+              consequatur enim architecto voluptates hic sequi vitae, dicta at
+              consectetur officiis earum tenetur.
+            </Card.Subtitle>
+            <Row>
+              <Col xs={1}>
+                <Form.Check
+                  {...register("check", {
+                    required: "Debe aceptar los terminos",
+                  })}
+                ></Form.Check>
+              </Col>
+              <Col xs={11}>
+                <p className="textForm">
+                  <strong>Acepto terminos y condiciones.</strong>
+                </p>
+              </Col>
+            </Row>
+          </Form.Group>
+          <Button className="mt-4 btnEntrar" variant="none" type="submit">
+            Crear cuenta
+          </Button>
+        </Form> */}
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group className="mt-4">
+            <Form.Label className="textForm">Nombres*</Form.Label>
+            <Form.Control
+              className="p-2 borderInput"
+              type="text"
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className="mt-4 mb-2">
+            <Form.Label className="textForm">Apellidos*</Form.Label>
+            <Form.Control
+              className="p-2 borderInput"
+              type="text"
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className="mt-4 mb-2">
+            <Form.Label className="textForm">Correo electronico*</Form.Label>
+            <Form.Control
+              className="p-2 borderInput"
+              type="email"
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className="mt-4 mb-2">
+            <Form.Label className="textForm">Contraseña*</Form.Label>
+            <Form.Control
+              className="p-2 borderInput"
+              type="password"
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Card.Subtitle className="mt-3 mb-3 textForm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+              deleniti sapiente alias nesciunt accusamus laborum doloribus
+              facere culpa. Quidem omnis velit necessitatibus. Commodi, animi
+              nesciunt. Illum consectetur amet placeat commodi. Lorem ipsum
+              dolor sit amet, consectetur adipisicing elit. Excepturi esse
+              maxime aspernatur libero, fuga explicabo molestias possimus
+              consequatur enim architecto voluptates hic sequi vitae, dicta at
+              consectetur officiis earum tenetur.
+            </Card.Subtitle>
+            <Row>
+              <Col xs={1}>
+                <Form.Check></Form.Check>
+              </Col>
+              <Col xs={11}>
+                <p className="textForm">
+                  <strong>Acepto terminos y condiciones.</strong>
+                </p>
+              </Col>
+            </Row>
+          </Form.Group>
+          <Button className="mt-4 btnEntrar" variant="none" type="submit">
+            Crear cuenta
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+};
 
-export default Registrarse
+export default Registrarse;
