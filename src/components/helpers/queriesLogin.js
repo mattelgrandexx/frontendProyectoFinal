9d2ecd8a1ee9c3ceaf0eed8ff9cffeb03bc3023b
nicoelias997 <nicoelias997@gmail.com>
2022-11-15 19:41:21 +0100

    Agrego las funciones para consultar lista de usuarios y para crear un nuevo usuario

---
+++ src/components/helpers/queriesLogin.js
@@ -0,0 +1,28 @@
+const URL = process.env.REACT_APP_API_RESTO_USER
+
+export const consultarUserApi = async () => {
+    try{
+        const respuesta = await fetch(URL)
+        const listaUsuarios = await respuesta.json()
+        console.log(listaUsuarios)
+        return listaUsuarios
+    } catch(e){
+        console.log(e)
+    }
+}
+
+export const crearUserApi = async (datos) => {
+    try{
+        const respuesta = await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(datos)
+        })
+        console.log(respuesta)
+        return respuesta
+    } catch(e){
+        console.log(e)
+    }
+}
