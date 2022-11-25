6da9d8a7d6295e1a7f749c0ce138681bec327bf1
nicoelias997 <nicoelias997@gmail.com>
2022-11-25 20:07:07 +0100

    Agrego estilo a mi componente Confirm, que es donde continuamos la confirmacion del email para iniciar sesion

--- src/components/views/Confirm.jsx
+++ src/components/views/Confirm.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import { Container } from 'react-bootstrap'
+import { Col, Container } from 'react-bootstrap'
 import { NavLink, useParams } from 'react-router-dom'
 import { obtenerUsuario } from '../helpers/queriesLogin'
 
 const Confirm = () => {
 
     const [usuario, setUsuario] = useState("")
+
     let { token } = useParams();
     
 
     useEffect(() => {
         obtenerUsuario(token).then(respuesta => {
             setUsuario({
+              nombreUsuario: respuesta.nombreUsuario,
                email:  respuesta.email,
                estado: respuesta.estado,
                _id: respuesta.id
@@ -19,10 +21,15 @@ const Confirm = () => {
         })
     }, [token])
   return (
-    <Container className='mainSection'>
-    <h1>Te registraste correctamente.</h1>
-    <p>Haz click en el siguiente enlace para continuar...</p>
-    <NavLink to={"/"}>Continuar..</NavLink>
+    <Container className='mainSection '>
+    <h1 className='pagPrincipal__titulo mt-5'>Te registraste correctamente.</h1>
+    <Col className='text-center'>
+       <h5 className='textUser'> Bienvenido, {usuario.nombreUsuario}.<br></br>
+        Podras iniciar sesion con el siguiente email: '{usuario.email}'
+        </h5>
+      <br></br>
+      <p className='textUser mt-4'>Haz click en el siguiente enlace para <NavLink to={"/login"} className="colorConfirm mt-2" >continuar..</NavLink></p>
+      </Col>
     </Container>
   )
 }
