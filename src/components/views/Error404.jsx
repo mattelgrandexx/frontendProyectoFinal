bc1adfb3365b47c468357cbb4c04e3ecc2af4722
matias cardoso <mattiascardoso10@gmail.com>
2022-11-30 22:48:34 -0300

    cambie el estilo y el maquetado en la pagina error

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Error404 = () => {
   return (
-    <div className='d-flex justify-content-center errorImagen'><img src="https://img.freepik.com/vector-gratis/ups-error-404-ilustracion-concepto-robot-roto_114360-5529.jpg?w=2000" alt="error404" /></div>
-  )
-}
+    <>
+    <section className="fondoError">
 
-export default Error404
+      <section className="fondoError d-flex justify-content-center">
+        <div className="text-center errorImagen divErrorImagen">
+          <img
+            src="https://img.freepik.com/vector-gratis/ups-error-404-ilustracion-concepto-robot-roto_114360-5529.jpg?w=2000"
+            className="w-100"
+            alt="error404"
+            />
+        </div>
+      </section>
+      <section>
+        <div className="fondoError text-center">
+          <h2 className="oswald400">No encontramos lo que estabas buscando, puedes regresar al inicio en el boton de abajo</h2>
+        </div>
+        <div className="d-flex justify-content-center fondoError mb-2">
+          <Link
+            to={"/"}
+            className="boton botonRegresarError oswald400 text-center"
+            >
+            REGRESAR
+          </Link>
+        </div>
+      </section>
+            </section>
+    </>
+  );
+};
+
+export default Error404;
